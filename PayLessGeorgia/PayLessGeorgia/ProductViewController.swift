442b80084471a5c459703c2740156b2b91d89e00
Irakli Chlagashvili <iraklichlagashvili@Iraklis-Mac.local>
2016-01-12 09:23:59 -0800

    Product Detail View Implementing

---
+++ PayLessGeorgia/PayLessGeorgia/ProductViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ProductViewController.swift
+//  PayLessGeorgia
+//
+//  Created by Irakli Chlagashvili on 1/12/16.
+//  Copyright © 2016 Vato Tabatadze. All rights reserved.
+//
+
+import Foundation
+
