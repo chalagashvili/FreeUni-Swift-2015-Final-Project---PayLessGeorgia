4e56ddc071c3ccfc12920e316eb366c96babf55a
vato20071 <vato20071@gmail.com>
2016-01-13 08:21:32 -0800

    Fixed minor bug when couldn't pass image to UIImageController

--- PayLessGeorgia/PayLessGeorgia/ProductDetailViewController.swift
+++ PayLessGeorgia/PayLessGeorgia/ProductDetailViewController.swift
@@ -11,19 +11,13 @@ import UIKit
 import SafariServices
 
 public class ProductDetailViewController: UIViewController,SFSafariViewControllerDelegate{
-    public var product: Product? {
-        didSet {
-
-        }
-    }
-    
+    public var product: Product?
     
     override public func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "Show Image" {
+        print(segue.identifier!)
+        if segue.identifier! == "Show Image" {
                 if let ivc = segue.destinationViewController as? ImageViewController {
-                    if let cell = sender as? ProductDetailViewController {
-                        ivc.img = cell.product?.img
-                    }
+                    ivc.img = product?.img
                 }
         }
     }
@@ -33,10 +27,21 @@ public class ProductDetailViewController: UIViewController,SFSafariViewControlle
     override public func viewDidLoad() {
         super.viewDidLoad()
         self.detailDescription.text = product!.desc
-        self.detailPriceChangeble.text = "" + (product?.price.description)!
+        self.detailPriceChangeble.text = "" + "\(round(Double((product?.price)!) * Double(currencyMap[prefferedCurrency!]!) * 100) / 100.0) \(prefferedCurrency!)"
         self.detailProductName.text = product?.name
-        self.detailProductImg.image = UIImage(contentsOfFile: (product?.name)!)
+        self.detailProductImg.image = product?.img
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "imageTapped:")
+        detailProductImg.addGestureRecognizer(tapGesture)
+        detailProductImg.userInteractionEnabled = true
     }
+    
+    func imageTapped(gesture: UIGestureRecognizer) {
+        if let _ = gesture.view as? UIImageView {
+            performSegueWithIdentifier("Show Image", sender: nil)
+        }
+    }
+    
     
    
     @IBAction func googler(sender: UIButton) {
@@ -52,11 +57,9 @@ public class ProductDetailViewController: UIViewController,SFSafariViewControlle
     
     @IBOutlet weak var detailPriceChangeble: UILabel!
     
-  
+    @IBOutlet weak var detailDescription: UITextView!
     
     @IBOutlet weak var detailProductName: UILabel!
     
-    
-    @IBOutlet weak var detailDescription: UILabel!
     @IBOutlet weak var detailProductImg: UIImageView!
 }
